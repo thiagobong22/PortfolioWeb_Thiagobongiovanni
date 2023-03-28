aacd41687f45d44065faf77d3c30277730f91ef6
thiagobong22 <117801113+thiagobong22@users.noreply.github.com>
2023-03-28 18:59:40 -0300

    Creacion del BackEnd
    Se creo en BackEnd

---
+++ Portfolioweb_Thiagobongiovanni/BackEnd/src/main/java/com/portfolio/thiagobong/ThiagobongApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.thiagobong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiagobongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThiagobongApplication.class, args);
+	}
+
+}
