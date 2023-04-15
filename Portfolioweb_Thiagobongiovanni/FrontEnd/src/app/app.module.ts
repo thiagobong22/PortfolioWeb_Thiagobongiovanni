5f091d3a5a8cf3806abb5097ce12815bee7bc523
thiagobong22 <117801113+thiagobong22@users.noreply.github.com>
2023-04-14 23:52:19 -0300

    Routing
    Se inicio el routing

--- Portfolioweb_Thiagobongiovanni/FrontEnd/src/app/app.module.ts
+++ Portfolioweb_Thiagobongiovanni/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import {HttpClientModule} from '@angular/common/http';
     EducacionComponent,
     HardysoftComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
